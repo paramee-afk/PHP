b8ad3cb83f8bee85457bf6bdfa9ce477e803146e
paramee-afk <67040233112@udru.ac.th>
2025-12-04 21:47:28 +0700

    Update PHP array.php

--- work/PHP array.php
+++ work/PHP array.php
@@ -1,72 +1,65 @@
 <!DOCTYPE html>
-<html lang="th">
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Php Array (เขียนวันที่ 4/12/2568)</title>
+    <meta charset="UTF-8">
+    <title>Php Array (เรียนวันที่ 4/12/2568)</title>
 </head>
 <body>
-
-  <h1>ทดสอบ Array แบบ Index Arrays</h1>
-  <?php
+    <?php
     $cars = array("Volvo", "BMW", "Toyota");
     echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
-  ?>
+    ?>
+    <h1> ทดสอบ Array แบบ Associative Arrays</h1>
+    <?php
+    $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43"); echo "Peter is " . $age['Peter'] . " years old.";
+    echo "Peter is " . $age['Peter'] . " years old," . "<br>";
+    echo "Ben is " . $age['Ben'] . " years old," . "<br>";
+    echo "Joe is " . $age['Joe'] . " years old," . "<br>";
+    echo "Mon is " . $age['Mon'] . " years old," . "<br>";
+?>
+<h1> การใช้ For กับ Index Arrays </h1>
+<?php
+$fruits = array("แอบเปิ้ล","มะละกอ", "กล้วย","ส้ม","องุ่น","มังคุด","ทุเรียน");
+$arrlen = count($fruits);
 
-  <h1>ทดสอบ Array แบบ Associative Arrays</h1>
-  <?php
-    $age = array(
-      "Peter" => "35",
-      "Ben"   => "37",
-      "Joe"   => "43",
-      "Mon"   => "19"
-    );
+for($x = 0; $x < $arrlen; $x++){
+    echo $fruits[$x]; 
+    echo "<br>\n";
+}
+?>
+<h1> การใช้ Foreach กับ Index Arrays </h1>
+<?php
+$fruits = array("แอบเปิ้ล","มะละกอ", "กล้วย","ส้ม","องุ่น","มังคุด","ทุเรียน");
+foreach($fruits as $value){
+    echo $value;
+    echo "<br>\n";
+}
+?>
+<h1> การใช้ Foreach กับ Associative Arrays </h1>
+<?php
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43", "Bas"=>"19");
 
-    echo "Peter is " . $age['Peter'] . " years old." . "<br>";
-    echo "Ben is "   . $age['Ben']   . " years old." . "<br>";
-    echo "Joe is "   . $age['Joe']   . " years old." . "<br>";
-    echo "Mon is "   . $age['Mon']   . " years old." . "<br>";
-  ?>
-
-  <h1>การใช้ foreach กับ Index Arrays</h1>
-  <?php
-    $fruits = array("แอปเปิ้ล", "มะละกอ", "กล้วย", "ส้ม", "องุ่น", "ทุเรียน");
-
-    foreach ($fruits as $value){
-        echo $value . "<br>";
-    }
-  ?>
-
-  <h1>การใช้ foreach กับ array แบบ Associative Arrays</h1>
-  <?php
-  $age = array(
-      "Peter" => "35",
-      "Ben"   => "37",
-      "Joe"   => "43",
-      "Mon"   => "19"
-  );
-
-  foreach ($age as $x => $x_value) {
-      echo $x . " มีอายุ " . $x_value . " ปี";
-      echo "<br>\n";
-  }
-  ?>
-
-<h1>การใช้ Array 2 มิติ (Two-Dimension Array)</h1>
-
+foreach($age as $x => $x_value){
+    echo $x . " มีอายุ " . $x_value . " ปี";
+    echo "<br>\n";
+}
+?>
+<h1> การใช้ Array 2 มิติ (Two Dimensional Arrays)</h1>
 <?php
 $cars = array(
-    array("Volvo", 22, 18),
-    array("BMW", 15, 13),
-    array("Saab", 5, 2),
-    array("Land Rover", 17, 15)
+    array("Volvo",22,18),
+    array("BMW",15,13,30,75),
+    array("Saab",5,2),
+    array("Land Rover",17,15,180)
 );
-$rows = count($cars); //นับจำนวนแถว
-for ($row = 0; $row < 4; $row++) {
+
+$rows = count($cars);
+for($row = 0; $row < $rows; $row++){
     echo "<p><b>Row number $row</b></p>";
     echo "<ul>";
-    $cols = count($cars[$row]); //นับจำนวนคอลัมน์ (บางกรณีข้อมูลไม่เท่ากัน)
-    for ($col = 0; $col < 3; $col++) {
-        echo "<li>".$cars[$row][$col]."</li>";
+    $cols = count($cars[$row]);
+    for($col = 0; $col < $cols; $col++){
+        echo "<li>" . $cars[$row][$col] . "</li>";
     }
     echo "</ul>";
 }
