388615c314b421448b0e9bbb44341691c7613808
paramee-afk <67040233112@udru.ac.th>
2025-12-04 21:49:51 +0700

    Update welcome get.php

--- work/welcome get.php
+++ work/welcome get.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
-
-<form action="welcome_get.php" method="get">
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
-    <input type="submit">
-</form>
-
+    Welcome <?php echo $_GET["name"]; ?><br>
+    Your email address is: <?php echo $_GET["email"]; ?>
 </body>
 </html>
