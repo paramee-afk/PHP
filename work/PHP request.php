a0dee831ce1df9bbc4ef6f225537520981491cd3
paramee-afk <67040233112@udru.ac.th>
2025-12-04 21:48:11 +0700

    Update PHP request.php

--- work/PHP request.php
+++ work/PHP request.php
@@ -5,18 +5,20 @@
     <title>PHP Request</title>
 </head>
 <body>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        Name : <input type="text" name="fname">
-        <input type="submit">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Name: <input type="text" name="fname" required>
+        Last Name: <input type="text" name="lname" required>
+        <input type="submit" value="ยืนยัน">
     </form>
-
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = $_REQUEST['fname'];
+        $name = htmlspecialchars($_POST['fname']);
+        $lastname = htmlspecialchars($_POST['lname']);
         if (empty($name)) {
             echo "Name is empty";
         } else {
-            echo $name;
+            echo "Name: " . $name . "<br>";
+            echo "Last Name: " . $lastname;
         }
     }
     ?>
