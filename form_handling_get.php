46d04d7db55f1b6e567860a666a7e158e4202565
paramee-afk <67040233112@udru.ac.th>
2025-12-19 14:00:45 +0700

    Add form handling GET PHP file

---
+++ form_handling_get.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="welcome_get.php" method="get">
+        Name : <input type="text" name="name">
+        <br>
+        Email : <input type="email" name="email">
+        <input type="submit" value="Submit">
+    </form>
+
+    
+</body>
+</html>
